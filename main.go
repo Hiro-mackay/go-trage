dc70e413fa87bd1c179f1098e2c9a2d098981c7d
mackay <johnmackay150@gmail.com>
2024-08-17 18:53:23 +0900

    logging

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
+func loggingSettings(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
+
 func main() {
-	file, _ := os.Open("./main.go")
-	defer file.Close()
-	data := make([]byte, 100)
-	file.Read(data)
-	fmt.Println(string(data))
+	loggingSettings("log.txt")
+	_, err := os.Open("undefined.txt")
+	if err != nil {
+		log.Fatalln("Exit", err)
+	}
+
 }
