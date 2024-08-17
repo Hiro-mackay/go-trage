c1e924631c327d0a682aba6906318d35defa6d20
mackay <johnmackay150@gmail.com>
2024-08-17 19:58:59 +0900

    type assertion

--- main.go
+++ main.go
@@ -2,44 +2,19 @@ package main
 
 import "fmt"
 
-type Human interface {
-	Say() string
-}
-
-type Person struct {
-	Name string
-}
-
-func (p *Person) Say() string {
-	p.Name = "Mr." + p.Name
-	fmt.Println("Hello", p.Name)
-	return p.Name
-}
-
-type Dog struct {
-	Name string
-}
-
-func (d Dog) Say() string {
-	fmt.Println("Bark")
-	return "Dog"
-}
-
-func Drive(human Human) {
-	if human.Say() == "Mr.Mike" {
-		fmt.Println("Drive")
-	} else {
-		fmt.Println("Get out")
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println("int", v)
+	case string:
+		fmt.Println("string", v)
+	default:
+		fmt.Printf("unknown type '%T'\n", v)
 	}
-
 }
 
 func main() {
-	var mike Human = &Person{Name: "Mike"}
-	Drive(mike)
-	var x Human = &Person{Name: "X"}
-	Drive(x)
-
-	var dog Dog = Dog{Name: "Dog"}
-	Drive(dog) // Ops! This is not a human. But Dog struct has Say() method. So, it is a Human interface.
+	do(21)
+	do("hello")
+	do(true)
 }
