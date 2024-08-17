dbfc23d7f8efebb42dc431e57141784709a60acf
mackay <johnmackay150@gmail.com>
2024-08-17 19:00:10 +0900

    panic&recover

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
+	"fmt"
 )
 
-func loggingSettings(logFile string) {
-	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multiLogFile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	log.SetOutput(multiLogFile)
+func connectDB() {
+	panic("Unable to connect database!")
 }
 
+func save() {
+	defer func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+	connectDB()
+}
 func main() {
-	loggingSettings("log.txt")
-	_, err := os.Open("undefined.txt")
-	if err != nil {
-		log.Fatalln("Exit", err)
-	}
+	save()
+	fmt.Println("OK?")
 
 }
