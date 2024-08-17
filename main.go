c4ed9856ffcc1639105e241d0fcb31966ff7905e
mackay <johnmackay150@gmail.com>
2024-08-17 19:10:53 +0900

    lesson2

--- main.go
+++ main.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
-func connectDB() {
-	panic("Unable to connect database!")
+func q1() {
+	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+	fmt.Println(slices.Min(l))
 }
 
-func save() {
-	defer func() {
-		s := recover()
-		fmt.Println(s)
-	}()
-	connectDB()
+func q2() {
+	m := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+
+	sum := 0
+
+	for _, v := range m {
+		sum += v
+	}
+
+	fmt.Println(sum)
+
 }
+
 func main() {
-	save()
-	fmt.Println("OK?")
-
+	q1()
+	q2()
 }
