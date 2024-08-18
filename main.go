1cc4cde3f4c9adf00470f1be034d1bc4add2b28e
mackay <johnmackay150@gmail.com>
2024-08-18 20:02:19 +0900

    iota

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/markcheno/go-quote"
-	"github.com/markcheno/go-talib"
+const (
+	_      = iota
+	KB int = 1 << (10 * iota)
+	MB
+	GB
 )
 
 func main() {
-	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
-	fmt.Print(spy.CSV())
-	rsi2 := talib.Rsi(spy.Close, 2)
-	fmt.Println(rsi2)
+	println(KB, MB, GB)
 }
