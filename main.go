ed9a079c14c389b5bd0a47944f71ee3898cfa865
mackay <johnmackay150@gmail.com>
2024-08-18 20:08:34 +0900

    context

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
-const (
-	_      = iota
-	KB int = 1 << (10 * iota)
-	MB
-	GB
+import (
+	"context"
+	"fmt"
+	"time"
 )
 
+func process(ctx context.Context, ch chan string) {
+	fmt.Println("Processing...")
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done!")
+	ch <- "Done"
+}
 func main() {
-	println(KB, MB, GB)
+	ch := make(chan string)
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	go process(ctx, ch)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		case v := <-ch:
+			fmt.Println("Received value: ", v)
+			return
+		}
+	}
 }
