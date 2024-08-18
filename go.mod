143ec2143e1ff08ddfa6387863b47b3bf24bc12d
mackay <johnmackay150@gmail.com>
2024-08-18 19:48:25 +0900

    3rd party

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-trade
 
 go 1.23.0
+
+require (
+	github.com/markcheno/go-quote v0.0.0-20240225224950-d942c652292c // indirect
+	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70 // indirect
+)
--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"go-trade/lib"
+
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
 )
 
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	fmt.Println(lib.Average(s))
-
-	person := lib.Person{Name: "John", Age: 25}
-	fmt.Println(person)
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
 }
