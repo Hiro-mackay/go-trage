faf3f3f8977dfe615224bbaa54cecbf9b0f6f3ec
mackay <johnmackay150@gmail.com>
2024-08-20 21:28:40 +0900

    balance, ticker

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"go-trade/bitflyer"
 	"go-trade/config"
 	"go-trade/utils"
-	"log"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
+
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 
 }
