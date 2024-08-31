ac9aee056baf5f542d269f3ccaa6bc3d41c4969a
mackay <johnmackay150@gmail.com>
2024-08-31 09:26:09 +0900

    feat: Add StreamIngestionData function for real-time data ingestion

---
+++ app/controllers/streamdate.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"go-trade/app/models"
+	"go-trade/bitflyer"
+	"go-trade/config"
+)
+
+func StreamIngestionData() {
+	var tickerChannel = make(chan bitflyer.Ticker)
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	go apiClient.GetRealTimeTicker(
+		config.Config.ProductCode, tickerChannel,
+	)
+	for ticker := range tickerChannel {
+		for _, duration := range config.Config.Durations {
+			isCreate := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
+			if isCreate && duration == config.Config.TradeDuration {
+				// TODO
+			}
+
+		}
+	}
+}
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"go-trade/app/models"
 	"go-trade/config"
+	"go-trade/controllers"
 	"go-trade/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	fmt.Println(models.DbConnection)
+	controllers.StreamIngestionData()
 }
